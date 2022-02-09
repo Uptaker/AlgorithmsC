e5edc04af4827419739b1af9e328f0e6b2dde9d1
Uptaker <markus.tammeoja@gmail.com>
2022-02-09 11:03:40 +0200

    Math library (sqrt, pow), triangle hypotenuse

--- 01 - C keel tutvustus/triangle.c
+++ 01 - C keel tutvustus/triangle.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
-main() {
+int main() {
     int side1, side2;
     printf("Please input the triangle base and height:\n");
     scanf("%d %d", &side1, &side2);
-    float area = (side1 * side2) / 2;
-    printf("Triangle area is %0.2f", area);
+    float area = (float) (side1 * side2) / 2;
+    float hypotenuse = sqrt(pow(side1, 2) + pow(side2, 2));
+    printf("Triangle area is %0.1f. Hypotenuse is %0.1f", area, hypotenuse);
 }
