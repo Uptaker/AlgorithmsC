22e9a194b3a1e0513d0de0db2083d464722401e1
Uptaker <dr1nky@tlu.ee>
2022-02-23 10:22:26 +0200

    refactor linked list

--- 03 - Pointers/linked2.c
+++ 03 - Pointers/linked2.c
@@ -6,36 +6,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct element {
+       int info;
+       struct element *next;
+};
 
-int main(void){
-    struct element {
-           int info; 
-           struct element *next;
-    };
-/* Kaks muutujat - head selleks, et seal ahela esimese elemendi aadressi säilitada ning node abimuutujaks uue elemendi lisamisel. */
-    struct element *head, *node, *first;
+
+void printNodes(struct element *current) {
+    while (current != NULL) {
+        printf("%d\n", current->info);
+        current = current->next;
+    }
+}
+
+struct element *readNotes() {
+    struct element *head, *node;
     int arv;
-    
     head = NULL;
 
     printf("Sisesta arv! (Lõpetamiseks 0)");
-/* Andmete sisestus (täisarvud!) töötab seni, kuni kasutaja trükib arvu 0 */
     scanf("%d",&arv);
     while (arv != 0){
-          node = malloc(sizeof *node); 
-          node->next = head; 	       
+          node = malloc(sizeof *node);
+          node->next = head;
           node->info = arv;
-          head = node; 		          
+          head = node;
           printf("Sisesta arv! (Lõpetamiseks 0)");
           scanf("%d",&arv);
     }
+    return head;
+}
 
-    printf("Now printing backwards:\n");
-    current = head;
-    while (current != NULL) {
-    	printf("%d\n", current->info);
-	current = current->next;
-    }
-
-
+int main(void){
+    struct element *head = readNotes();
+    printNodes(head);
 }
