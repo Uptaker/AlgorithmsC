fe378a983612352ff47cdf5efe1233dac6ddeeab
Uptaker <markus.tammeoja@gmail.com>
2022-03-02 10:50:50 +0200

    Polish notation

--- 04 - Ahelad ja stringid/polishNotation.c
+++ 04 - Ahelad ja stringid/polishNotation.c
@@ -32,20 +32,35 @@ int stackPop(void) {
 main() {
     int number, i, length;
     char expression[50];
-    printf("Inputs infiks expression");
+    printf("Input infiks expression ");
     scanf("%49s", expression);
     getchar();
     length = strlen(expression);
     stackInit(length);
 
     // reversed Polish notation
+    printf("\npostfix: ");
     for (i = 0; i < length; i++) {
         if (expression[i] == '(') continue;
         if (expression[i] == '+' || expression[i] == '-' ||
             expression[i] == '*' || expression[i] == '/') {
             stackPush(expression[i]);
-        } else if (expression[i] == ')') {
-            printf("%c ", stackPop());
-        } else printf("%c ", expression[i]);
+        } else if (expression[i] == ')') printf("%c ", stackPop());
+        else printf("%c ", expression[i]);
+    }
+
+    stackInit(length);
+
+    // Polish notation - TODO fix wrong order
+    printf("\nprefix: ");
+    for (i = 0; i < length; i++) {
+        if (expression[i] == '+' || expression[i] == '-' ||
+            expression[i] == '*' || expression[i] == '/') stackPush(expression[i]);
+    }
+    for (i = 0; i < length; i++) {
+        if (expression[i] == ')' || expression[i] == '+' || expression[i] == '-' ||
+            expression[i] == '*' || expression[i] == '/') continue;
+        if (expression[i] == '(') printf("%c ", stackPop());
+        else printf("%c ", expression[i]);
     }
 }
