c5d55981d60d6e6bedcf59680d12a01d1dc94d11
Uptaker <markus.tammeoja@gmail.com>
2022-05-11 11:13:39 +0300

    make algorithm more resilient to cases where picking the high pivot every time is not going to be efficient

--- 11 - Sort/quicksort.c
+++ 11 - Sort/quicksort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void swap(int *x, int *y);
 void quicksort(int array[], int length);
@@ -25,6 +27,7 @@ void swap(int *x, int *y) {
 }
 
 void quicksort(int array[], int length) {
+    srand(time(NULL));
     quicksort_recursion(array, 0, length -1);
 }
 
@@ -37,6 +40,12 @@ void quicksort_recursion(int array[], int low, int high) {
 }
 
 int partition(int array[], int low, int high) {
+    int pivot_index = low + (rand() % (high - low));
+
+    if (pivot_index != high) {
+        swap(&array[pivot_index], &array[high]);
+    }
+
     int pivot = array[high];
     int i = low;
     for (int j = low; j < high; j++) {
