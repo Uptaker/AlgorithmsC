87d61a238aaf7800223624caf697b69ecbac71ef
Uptaker <markus.tammeoja@gmail.com>
2022-05-18 10:33:09 +0300

    add hash table with lookup, insert and delete

---
+++ 12 - HashTable/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+
+    printf("Jacob => %u\n", hash("Jacob"));
+    printf("Bill => %u\n", hash("Bill"));
+    printf("Natalie => %u\n", hash("Natalie"));
+    printf("Sara => %u\n", hash("Sara"));
+    printf("Mpho => %u\n", hash("Mpho"));
+    printf("Jane => %u\n", hash("Jane"));
+    printf("Maren => %u\n", hash("Maren"));
+    printf("Ron => %u\n", hash("Ron"));
